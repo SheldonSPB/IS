902aab7a5c15237fd7c5d2679533d9137d31dc5a
Artem <sabakababaka459@gmail.com>
2025-03-03 11:07:40 +0000

    qwe

--- README.cpp
+++ README.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    setlocale(LC_ALL, "rus");
+    cout << "Сколько тебе лет?" << endl;
+    cin >> a;
+    if (a < 18){
+        cout << "Доступ запрещён!" << endl;
+    }
+    else cerror << "Доступ открыт!" << endl;
+    return 0;
+}
 
